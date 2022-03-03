90eecb859b662587360ccc38c35df65ef6c000f4
Shane Todd <smt531@gmail.com>
2022-03-03 09:40:46 -0500

    setting up solution structure

---
+++ InvestmentPerformance/InvestmentPerformance.Business/Models/Listing.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvestmentPerformance.Business.Models
+{
+    public class Listing
+    {
+        public int Id { get; set; }
+
+        public string CompanyName { get; set; }
+
+        public decimal CurrentPrice { get; set; }
+    }
+}
---
+++ InvestmentPerformance/InvestmentPerformance.Business/Models/UserInvestment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvestmentPerformance.Business.Models
+{
+    public class UserInvestment
+    {
+        public int Id { get; set; }
+
+        public int ListingId { get; set; }
+
+        public int UserId { get; set; }
+
+        public decimal Shares { get; set; }
+
+        public decimal SharePurchasePrice { get; set; }
+
+        public DateTime PurchaseDate { get; set; }
+    }
+}
