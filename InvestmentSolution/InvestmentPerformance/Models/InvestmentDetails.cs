d4161c7ffcc2d4425cc17e6b2bdc8a0eb2fdd8b9
Supriti Ghosh <supritighosh@Supritis-MacBook-Pro.local>
2023-03-04 01:55:40 -0500

    Updated code

---
+++ InvestmentSolution/InvestmentPerformance/Models/InvestmentDetails.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InvestmentPerformance.Models
+{
+	public class InvestmentDetails
+	{
+        [Key]
+        public Guid OwnershipId { get; set; }
+
+        public decimal CostBasis { get; set; }
+        public decimal CurrentPrice { get; set; }
+        public int NumberOfShares { get; set; }
+
+        public DateTime PurchaseDateTime { get; set; }
+
+        public decimal CurrentValue => NumberOfShares * CurrentPrice;
+        public decimal GainLoss => CurrentValue - (CostBasis * NumberOfShares);
+
+        public string Term => (PurchaseDateTime.AddYears(1) >= DateTime.Now) ? "Short Term" : "Long Term";
+
+        public Guid InvestmentId { get; set; }
+        public Guid UserId { get; set; }
+
+    }
+}
+
---
+++ InvestmentSolution/InvestmentPerformance/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace InvestmentPerformance.Models
+{
+    public class User
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+
+    }
+}
+
